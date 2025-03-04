844031080ca64e8e1b3e34525a53308dfaa38d73
topcualperen <alperenttpc78@gmail.com>
2025-03-04 07:54:27 +0300

    2011. Final Value of Variable After Performing Operations

---
+++ leetcode/2011_FinalValueofVariableAfterPerformingOperations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        
+        int sum = 0;
+
+        for (int i = 0; i < operations.length; i++){
+            if (operations[i].equals("--X") || operations[i].equals("X--")) sum --;
+            if (operations[i].equals("++X") || operations[i].equals("X++")) sum ++;
+        }
+
+        return sum;
+    }
+}
