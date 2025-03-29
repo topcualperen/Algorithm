252aea081a517c80f18caf73aeea6060bf7c1086
topcualperen <alperenttpc78@gmail.com>
2025-03-29 16:47:28 +0300

    VariableSubstitution Update

--- other/VariableSubstitution.java
+++ other/VariableSubstitution.java
@@ -7,9 +7,10 @@ public class VariableSubstitution{
         int a = 5;
         int b = 7;
 
-        a = a + b;  // a = 5 + 7    a -> 12
+        a += b;  // a = 5 + 7    a -> 12
         b = a - b;  // b = 12 - 7   b -> 5
-        a = a - b;  // a = 12 - 5   a -> 7
+        a -= b;  // a = 12 - 5   a -> 7
 
     }
 }
+// source: https://github.com/Mulakat-Adam/java-mulakat-sorulari/blob/main/src/Q13.java
