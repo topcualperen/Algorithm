54fd5c3b1c7c7641b75e6872a10bd1075243dae9
topcualperen <alperenttpc78@gmail.com>
2025-03-29 16:41:28 +0300

    Variable Substitution

---
+++ other/VariableSubstitution.java
@@ -0,0 +1,15 @@
+
+
+public class VariableSubstitution{
+
+    public static void main(String[] args){
+
+        int a = 5;
+        int b = 7;
+
+        a = a + b;  // a = 5 + 7    a -> 12
+        b = a - b;  // b = 12 - 7   b -> 5
+        a = a - b;  // a = 12 - 5   a -> 7
+
+    }
+}
