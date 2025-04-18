1389cc5e64291eab476eecd5f65a196d9ecb9d5d
topcualperen <alperenttpc78@gmail.com>
2025-04-18 15:30:10 +0300

    Update 1768. Merge Strings Alternately

--- leetcode/1768_MergeStringsAlternately.java
+++ leetcode/1768_MergeStringsAlternately.java
@@ -1,20 +1,20 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         
-        int wordLength = 0;
-            if (word1.length() > word2.length()) wordLength = word1.length();
-            else wordLength = word2.length();
+        int longWordLen = 0;
+        if (word1.length() > word2.length()) longWordLen = word1.length();
+        else longWordLen = word2.length();
+
+        // abc - xyz -> axbycz
+
+        String outputString = "";
     
-            String output = "";
-    
-            for (int i = 0; i < wordLength; i++) {
-                if (word1.length() > i){
-                    output += word1.charAt(i);
-                }
-                if (word2.length() > i){
-                    output += word2.charAt(i);
-                }
-            }
-            return output;
+        for (int i = 0; i < longWordLen; i++){
+
+            if (word1.length() > i) outputString += word1.charAt(i);
+            if (word2.length() > i) outputString += word2.charAt(i);
+        }
+
+        return outputString;
     }
 }
