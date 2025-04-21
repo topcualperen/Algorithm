7944a8236e47451ba7c994d691abff0629918eff
topcualperen <alperenttpc78@gmail.com>
2025-04-22 00:08:17 +0300

    Intersection Of Two Arrays

--- other/claude/IntersectionOfTwoArrays.java
+++ other/claude/IntersectionOfTwoArrays.java
@@ -8,27 +8,31 @@ public class IntersectionOfTwoArrays {
         Set<Integer> set = new HashSet<>();
         
         for (int i = 0; i < nums1.length; i++){
-            set.add(nums[i]);
+            set.add(nums1[i]);
         }
 
         Set<Integer> resultSet = new HashSet<>();
 
-        for (int i = 0; i < nums2.length; i++){
-            if (set.contains(nums2[i])) resultSet.add(nums2[i]);
+        for (int j = 0; j < nums2.length; j++){
+            if (set.contains(nums2[j])) resultSet.add(nums2[j]);
         }
 
         int[] resultArr = new int[resultSet.size()];
         int i = 0;
         for (int num : resultSet){
-            resultArr[i++] = i; 
+            resultArr[i++] = num; 
         }
+
+        return resultArr;
     }
 
     public static void main(String[] args){
 
-        int[] nums1 = {1,2,2,1};
-        int[] nums2 = {1,5,6,6,9};
+        int[] nums1 = {51,2,2,1};
+        int[] nums2 = {1,51,6,6,9};
 
-        System.out.println(intersectionofTwoArrays(nums1, nums2));
+        for (int i : intersectionofTwoArrays(nums1, nums2)){
+            System.out.println(i);
+        }
     }
 }
