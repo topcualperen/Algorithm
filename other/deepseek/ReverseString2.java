9b538ceace17c704f78c8478d9d7d8518917eef5
topcualperen <alperenttpc78@gmail.com>
2025-04-23 17:56:00 +0300

    Reverse String 2 (in-place)

---
+++ other/deepseek/ReverseString2.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+// Bir karakter dizisini (char[] s) yerinde (in-place) ters çevirin. Ekstra bellek kullanmayın.
+
+public class ReverseString2{
+
+    private static void reverseString(char[] s){
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while (right > left){
+            
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left ++;
+            right --;
+        }
+    }
+
+    public static void main(String[] args){
+
+        char[] s = {'h','e','l','l','o'};
+
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+}
