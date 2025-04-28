b53daba1d337fb6a910e94d6dd004ab5adc42840
topcualperen <alperenttpc78@gmail.com>
2025-04-28 15:41:02 +0300

    Add description - String Reverse

--- other/claude/StringReversal.java
+++ other/claude/StringReversal.java
@@ -13,3 +13,25 @@ public class StringReversal {
         System.out.println("Reversed Word: " + reverseString(new String("Hello World")));
     }
 }
+
+/*
+
+    Bu şekilde çözüm olursa,
+
+    Java'da String nesneleri değişmezdir. İlk algoritmadaki reverseWord += word.charAt(i) işlemi 
+    her iterasyonda yeni bir String nesnesi oluşturur ve bu, uzun metinlerde ciddi performans sorunlarına yol açar.
+
+
+   private static String reverseString(String word){
+
+    String reverseWord = "";
+
+    for (int i = word.length() - 1; i >= 0; i--){
+
+        reverseWord += word.charAt(i);
+    }
+
+    return reverseWord;
+}
+  
+ */
