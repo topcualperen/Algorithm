afcc94a89e69f666320a9c5037091116e54b57e3
topcualperen <alperenttpc78@gmail.com>
2025-05-05 00:04:37 +0300

    Update Two Sum

--- leetcode/1_TwoSum.java
+++ leetcode/1_TwoSum.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] arr = new int[2];
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++){
-            
-            if (map.containsKey(target - nums[i])){
-                arr[0] = map.get(target - nums[i]);
-                arr[1] = i;
-                break;
+
+            if (map.containsKey(target - nums[i])) {
+
+                return new int[]{map.get(target - nums[i]), i};
             }
-            map.put(nums[i], i); // 2-0, 7-1, 11-2, 15-3
+
+            map.put(nums[i], i);
         }
-        return arr;
+
+        return new int[] {};
     }
 }
