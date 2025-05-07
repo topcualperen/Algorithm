2798edc6e8c1101106d426d3a27d69eefc2dc33a
topcualperen <alperenttpc78@gmail.com>
2025-05-07 17:58:12 +0300

    update prime -> java

--- other/Prime.java
+++ other/Prime.java
@@ -2,9 +2,8 @@ public class Prime{
     
     private static boolean isPrime(int number){
 
-        if (number < 2) return false;
+        if (number < 2 || number % 2 == 0) return false;
         if (number == 2 || number == 3) return true;
-        if (number % 2 == 0) return false;
         
         for (int i = 3; i <= Math.sqrt(number); i+=2){
 
