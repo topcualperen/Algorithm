a3af17d383d35ec7562033b90fb5f7b0b67ab668
topcualperen <alperenttpc78@gmail.com>
2025-05-10 18:50:59 +0300

    Improve my solution - Palindrome -> Java

--- leetcode/9_PalindromeNumber.java
+++ leetcode/9_PalindromeNumber.java
@@ -7,12 +7,12 @@ class Solution {
         int number = x;
 
         while (number > 0){
-            int step = (int) number % 10;
-            reverseNumber = reverseNumber * 10 + step;
+            int remainder = (int) number % 10;
+            reverseNumber = reverseNumber * 10 + remainder;
             number /= 10;
         }
 
-        if (reverseNumber == x) return true;
-        else return false;
+        return reverseNumber == x;
+
     }
 }
