d275fda466de4879ce822ed1fa8c0324419b4958
topcualperen <alperenttpc78@gmail.com>
2025-05-19 17:12:55 +0300

    Container With Most Water

---
+++ other/chatGPT/ContainerWithMostWater.java
@@ -0,0 +1,34 @@
+
+public class ContainerWithMostWater{
+
+    /* 
+        Yükseklikleri height[i] olan dikey çizgilerden bir dizi veriliyor. 
+        İki çizgi seçip, x-ekseniyle arasında su tutabilecek maksimum alanı bulun.
+    */
+
+    private static int maxArea(int[] height){
+
+        int area = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (right > left){
+
+            int w = right - left;
+            int h = Math.min(height[right], height[left]);
+
+            area = Math.max(area, h * w);
+
+            if (height[right] > height[left]) left ++;
+            else right --;
+        }
+
+        return area;
+    }
+
+    public static void main(String[] args){
+
+        int[] nums = new int[] {8,9,5,4,4,8,3,2};
+        System.out.println(maxArea(nums));
+    }
+}
