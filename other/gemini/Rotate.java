2c53761d0d814bb09c4c2fa7c4c8f0558cdcab78
topcualperen <alperenttpc78@gmail.com>
2025-07-03 16:20:30 +0300

    Added Description

--- other/gemini/Rotate.java
+++ other/gemini/Rotate.java
@@ -1,3 +1,13 @@
+
+/* 
+    Verilen bir nums dizisini, k adım sağa döndürün. k negatif olmayan bir tamsayıdır.
+
+    Örnek:
+
+        Girdi: nums = [1,2,3,4,5,6,7], k = 3
+        Çıktı: [5,6,7,1,2,3,4]
+*/
+
 import java.util.Arrays;
 
 public class Rotate {
