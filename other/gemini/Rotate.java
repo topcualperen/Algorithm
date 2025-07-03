4e81f4fbf474a8239f65379ab05344d95e070e2e
topcualperen <alperenttpc78@gmail.com>
2025-07-03 16:18:12 +0300

    Rotate Array

---
+++ other/gemini/Rotate.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Rotate {
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (end > start) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            end--;
+            start++;
+        }
+    }
+
+    private static int[] rotate(int[] nums, int k) {
+
+        k %= nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        int number = 3;
+
+        System.out.println(Arrays.toString(rotate(arr, number)));
+    }
+}
