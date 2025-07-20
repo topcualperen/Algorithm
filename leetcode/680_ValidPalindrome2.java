45c40aeb947cbbbb99f7003cc1e218faf8de4bef
topcualperen <alperenttpc78@gmail.com>
2025-07-20 16:35:22 +0300

    feat(leetcode): add solution for 680. Valid Palindrome 2 in java

--- leetcode/680_ValidPalindrome2.java
+++ leetcode/680_ValidPalindrome2.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean validPalindrome(String s) {
 
-        int left = 0, right = s.length() - 1;
+        int left = 0;
+        int right = s.length() - 1;
 
         while (right > left) {
 
