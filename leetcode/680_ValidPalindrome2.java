49dfaeda6c305d8b430739f131c066e949347607
topcualperen <alperenttpc78@gmail.com>
2025-07-20 16:28:10 +0300

    680 Valid Palindrome 2

---
+++ leetcode/680_ValidPalindrome2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+
+        int left = 0, right = s.length() - 1;
+
+        while (right > left) {
+
+            if (s.charAt(left) == s.charAt(right)) {
+                left ++;
+                right --;
+            }
+            else {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPalindrome(String s, int left, int right) {
+
+        while (right > left) {
+
+            if (s.charAt(right) != s.charAt(left)) {
+                return false;
+            }
+            left ++;
+            right --;
+        }
+        return true;
+    }
+}
