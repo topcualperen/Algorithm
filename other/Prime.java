d1d877474cf40e78836c8cf974e690365c5985a5
topcualperen <alperenttpc78@gmail.com>
2025-08-01 18:34:39 +0300

    refactor(Prime): improve code formatting and simplify isPrime method

--- other/Prime.java
+++ other/Prime.java
@@ -1,22 +1,22 @@
-public class Prime{
-    
-    private static boolean isPrime(int number){
 
+
+public class Prime {
+    
+    private static boolean isPrime(int number) {
+
+        if (number == 2 ||number == 3) return true;
         if (number < 2 || number % 2 == 0) return false;
-        if (number == 2 || number == 3) return true;
-        
-        for (int i = 3; i <= Math.sqrt(number); i+=2){
+
+        for (int i = 5; i <= Math.sqrt(number); i +=2) {
 
             if (number % i == 0) return false;
         }
-
         return true;
     }
     
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int num = 17;
-
-        System.out.println("is prime: " + isPrime(num));
+        int number = 2;
+        System.out.println(isPrime(number));
     }
 }
