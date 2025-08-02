5c1af9fd8861702811087925c95542bbcde0d40f
topcualperen <alperenttpc78@gmail.com>
2025-08-02 06:18:38 +0300

    refactor(TheLargestElementInArray): improve logic and formating in largestElement method

--- other/deepseek/TheLargestElementInArray.java
+++ other/deepseek/TheLargestElementInArray.java
@@ -2,15 +2,17 @@
 public class TheLargestElementInArray {
     
 
-    private static int largestElement(int[] arr){
+    private static int largestElement(int[] arr) {
+
+        if (arr.length < 1 || arr == null) {
+            throw new IllegalArgumentException("The array must contain at least 1 number");
+        }
 
         int biggestNumber = arr[0];
 
-        for (int i = 1; i < arr.length; i++){
-            
-            if (arr[i] > biggestNumber) biggestNumber = arr[i];
+        for (int i : arr) {
+            if (i > biggestNumber) biggestNumber = i;
         }
-
         return biggestNumber;
     }
 
