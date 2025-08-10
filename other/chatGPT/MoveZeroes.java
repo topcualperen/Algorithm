f07df6ed06e645e2a56f7d55c004f151bf8419eb
topcualperen <alperenttpc78@gmail.com>
2025-08-10 15:39:21 +0300

    refactor(MoveZeroes): improve code formatting

--- other/chatGPT/MoveZeroes.java
+++ other/chatGPT/MoveZeroes.java
@@ -1,35 +1,31 @@
 
 
 /*
- * 
- *  Verilen bir tamsayı dizisi nums, dizideki tüm sıfırları koruyarak 
- *  diğer tüm öğelerin görece sırasını bozmadan dizinin sağ tarafına taşıyın. 
+ *
+ *  Verilen bir tamsayı dizisi nums, dizideki tüm sıfırları koruyarak
+ *  diğer tüm öğelerin görece sırasını bozmadan dizinin sağ tarafına taşıyın.
  *  Bunu yerinde (in-place), ekstra dizi kullanmadan yapın.
- * 
+ *
  */
 
 public class MoveZeroes {
-    
-    private static int[] moveZeroes(int[] nums){
-
-        int temp = 0;
-
-        for (int i = 0; i < nums.length; i++){
-            
-            if (nums[i] != 0) nums[temp++] = nums[i];
+    private static int[] moveZeroes(int[] nums) {
+        if (nums == null || nums.length <= 1) return nums;
+        int writeIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[writeIndex++] = nums[i];
+            }
         }
-
-        for (int i = temp; i < nums.length; i++){
+        for (int i = writeIndex; i < nums.length; i++) {
             nums[i] = 0;
         }
-
         return nums;
     }
 
+
     public static void main(String[] args){
-
-        int[] nums = new int[] {1,6,0,3,12,0};
-
+        int[] nums = new int[] {1,6,0,3,12,0}; // 1, 6, 3, 12, 0, 0
         for (int i : moveZeroes(nums)){
             System.out.print(i + " ");
         }
