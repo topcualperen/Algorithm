e61c5fb644d58cdb6f4c36683cb8082d33bafd5d
alperen <alperenttpc78@gmail.com>
2025-08-17 15:53:55 +0300

    refactor(DigitCounter): improve code formatting

--- other/chatGPT/DigitCounter.java
+++ other/chatGPT/DigitCounter.java
@@ -1,19 +1,17 @@
+package chatGPT;
+
 public class DigitCounter {
  
     private static int digitCounter(int num){
-
         int counter = 0;
-
         while (num > 0) {
             num /= 10;
             counter ++;
         }
-
         return counter;
     }
 
     public static void main(String[] args) {
-
         int number = 8654;
         System.out.println(digitCounter(number));
     }
